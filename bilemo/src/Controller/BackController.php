ce0a69ea7b074726fcfe9c4179f4c04f86739679
gregory castaing <graboiide@gmail.com>
2020-08-11 20:43:59 +0200

    add order and filter, refactoring

--- bilemo/src/Controller/BackController.php
+++ bilemo/src/Controller/BackController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Repository\UserRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,15 +18,17 @@ class BackController extends AbstractController
     protected $serializer;
     protected $params ;
     protected $request;
+    protected $group ;
     public function __construct(SerializerInterface $serializer )
     {
         $this->serializer = $serializer;
     }
 
-    function links($entity,$params,$json = true,$groups = null)
+    function links($entity,$params,$json = true)
     {
-
-        $userJson = $this->serializer->serialize($entity,'json',['groups'=>$groups]);
+        if(is_null($entity))
+            return null;
+        $userJson = $this->serializer->serialize($entity,'json',['groups'=>$this->group]);
         $userAssoc = json_decode($userJson,true);
         $links = [];
         foreach ($params as $key => $param){
@@ -40,23 +43,21 @@ class BackController extends AbstractController
         return $userAssoc;
     }
 
-    /**
-     * Recupere les données en fonction du numéro de page
-     * @param PaginatorInterface $paginator
-     * @param $data
-     * @param Request $request
-     * @return PaginationInterface
-     */
-    protected function dataForPage(PaginatorInterface $paginator,$data,Request $request)
+    protected function getData(Request $request,ServiceEntityRepository $repo,$criteria = [])
     {
-        $limit=count($data);
-        //pagination
-        if(!is_null($request->get('page')))
-            $limit = 10;
-        return $paginator->paginate($data,$request->query->getInt('page',1),$request->query->getInt('page_size',$limit));
+        $limit = $request->query->getInt('page_size',$repo->count([]));
+        $page = max(0,$request->query->getInt('page',1));
+        $order = $request->query->getAlpha('order','ASC');
+        $filter = $request->query->getAlpha('filter','id');
+        $entities = $repo->findBy($criteria,[$filter=>$order],$limit,($page-1)*$limit);
+
+        $all = [];
+        foreach ($entities as $entity){
+            $all[]= $this->links($entity,$this->params,false);
+        }
+        return $all;
 
     }
-
     /**
      * Active le httpcache de symfony avec validation depuis le Etag
      * @param Response $response
